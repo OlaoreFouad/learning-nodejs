4e856b4e911eba4430b7c2a3f01c3614989dccee
Foodie <olaorefouad565@gmail.com>
2021-02-02 01:27:33 +0400

    learnt how to use the on callback to track data and end events in the response object, provided an avenue to access the data but not been able to write the received data into a file for some reason

--- node-basics/app.js
+++ node-basics/app.js
@@ -1,8 +1,56 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    process.exit();
+
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>NodeJS Basics</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit" >Send</button></form></body>');
+        res.write('</html>');
+
+        return res.end();
+    }
+
+    // create a file with dummy text
+    if (req.url === '/message' && method === 'POST') {
+
+        console.log('got here');
+
+        const body = [];
+        res.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        })
+
+        res.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+
+            console.log(parsedBody);
+
+            fs.writeFileSync('message.txt', 'new message');
+        })
+
+        // res.writeHead(302, '', { 'Location': '/' });
+        // res.statusCode = 302;
+        // res.setHeader('Location', '/');
+
+        return res.end();
+    }
+
+    // send a response
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>NodeJS Basics</title></head>');
+    res.write('<body><h1>Welcome to my Node.js Server</h1></body>')
+    res.write('</html>');
+
+    res.end();
+
 })
 
 server.listen(3000);
