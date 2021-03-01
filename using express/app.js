e41e89ba0f94c772e76e7b0a75e89c4a8f7e5cd8
Foodie <olaorefouad565@gmail.com>
2021-03-01 21:00:28 +0400

    learnt how to use body-parser package to parse incoming requests by using it as a middleware, also used the express router to handle and destructure routing information into separate files

--- using express/app.js
+++ using express/app.js
@@ -1,20 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs!')
-    next()
-})
-
-app.use('/add-product', (req, res, next) => {
-    console.log('In "Add Product" middleware')
-    res.send('<h1>Welcome to "Add Product" Page</h1>')
-})
-
-app.use('/', (req, res, next) => {
-    console.log('In first middleware!')
-    res.send('<h1>Hello from Express!</h1>')
-})
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(adminRoutes)
+app.use(shopRoutes)
 
 app.listen(3000);
