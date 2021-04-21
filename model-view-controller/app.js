1983043b406b7e1109029e2d18d8bd78e3cd856c
Olaore Fouad <olaorefouad565@gmail.com>
2021-04-21 12:34:11 +0400

    feature: worked on syncing JS model definitions to mysql database using sequelize. also used the create method to add a product using sequelize

--- model-view-controller/app.js
+++ model-view-controller/app.js
@@ -1,28 +1,34 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const root = require('./utils/path')
-const db = require('./utils/database')
+const root = require("./utils/path");
+const db = require("./utils/database");
 
 // controllers
-const utilControllers = require('./controllers/utils')
+const utilControllers = require("./controllers/utils");
+const sequelize = require("./utils/database");
 
 // routes
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(root, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(root, "public")));
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.use('/', utilControllers.getPageNotFound)
+app.use("/", utilControllers.getPageNotFound);
+
+sequelize
+  .sync()
+  .then((result) => console.log(result))
+  .catch((err) => console.error(err));
 
 app.listen(3000);
--- model-view-controller/controllers/products.js
+++ model-view-controller/controllers/products.js
@@ -1,79 +1,83 @@
-const Product = require('../models/product')
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-    const payload = {
-        path: '/admin/add-product',
-        pageTitle: 'Add Product',
-        activeAddProduct: true,
-        productCss: true,
-        editing: false
-    }
-    res.render('admin/edit-product', payload)
-}
+  const payload = {
+    path: "/admin/add-product",
+    pageTitle: "Add Product",
+    activeAddProduct: true,
+    productCss: true,
+    editing: false,
+  };
+  res.render("admin/edit-product", payload);
+};
 
 exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const price = req.body.price;
+  const imageUrl = req.body.imageUrl;
 
-    const title = req.body.title;
-    const description = req.body.description;
-    const price = req.body.price;
-    const imageUrl = req.body.imageUrl;
-
-    const product = new Product(null, title, description, price, imageUrl)
-    product.save()
-        .then(() => {
-            res.redirect('/')
-        })
-        .catch(err => console.error(err))
-}
+  Product.create({
+    title,
+    description,
+    price,
+    imageUrl,
+  })
+    .then((result) => console.log(result))
+    .catch((err) => console.error(err));
+};
 
 exports.getEditProduct = (req, res, next) => {
-    const productId = req.params.productId;
-    const editMode = req.query.edit;
+  const productId = req.params.productId;
+  const editMode = req.query.edit;
 
-    if (!editMode) {
-        return res.redirect('/')
+  if (!editMode) {
+    return res.redirect("/");
+  }
+
+  Product.findById(productId, (product) => {
+    if (!product) {
+      res.redirect("/");
     }
 
-    Product.findById(productId, product => {
-
-        if (!product) {
-            res.redirect('/');
-        }
-
-        const payload = {
-            path: '/admin/edit-product',
-            pageTitle: 'Edit Product',
-            editing: editMode,
-            product: product
-        }
-        res.render('admin/edit-product', payload)
-    })
-
-}
+    const payload = {
+      path: "/admin/edit-product",
+      pageTitle: "Edit Product",
+      editing: editMode,
+      product: product,
+    };
+    res.render("admin/edit-product", payload);
+  });
+};
 
 exports.postEditProduct = (req, res, next) => {
-    const productId = req.body.productId;
-    const updatedProduct = new Product(productId, req.body.title, req.body.description, req.body.price, req.body.imageUrl);
+  const productId = req.body.productId;
+  const updatedProduct = new Product(
+    productId,
+    req.body.title,
+    req.body.description,
+    req.body.price,
+    req.body.imageUrl
+  );
 
-    updatedProduct.save()
-    res.redirect('/admin/products')
-
-}
+  updatedProduct.save();
+  res.redirect("/admin/products");
+};
 
 exports.getAdminProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        const payload = {
-            path: '/admin/products',
-            pageTitle: 'Admin Products',
-            prods: products
-        }
-        res.render('admin/view-products', payload)
-    })
-}
+  Product.fetchAll((products) => {
+    const payload = {
+      path: "/admin/products",
+      pageTitle: "Admin Products",
+      prods: products,
+    };
+    res.render("admin/view-products", payload);
+  });
+};
 
 exports.postDeleteProduct = (req, res, next) => {
-    const productId = req.body.productId;
-    Product.delete(productId, () => {
-        res.redirect('/admin/products')
-    })
-}
+  const productId = req.body.productId;
+  Product.delete(productId, () => {
+    res.redirect("/admin/products");
+  });
+};
